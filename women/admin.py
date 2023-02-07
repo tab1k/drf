72ab6474101cca3fb4e07d998d7a382ecfbc44db
tab1k <tabigatkarbayev@icloud.com>
2023-02-08 01:38:07 +0600

    first commit

---
+++ women/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from women.models import Women
+
+# Register your models here.
+
+
+admin.site.register(Women)
---
+++ women/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from women.models import Women
+from women.serializers import WomenSerializer
+
+
+# Create your views here.
+
+
+class WomenAPIView(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
